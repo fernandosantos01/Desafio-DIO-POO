3ecde992b75a52cbe801f57ed4af71a54f61e2c1
PC DA MARIA EDUARDA <josefernandojosefernando.09@gmail.com>
2025-03-24 19:29:11 -0300

    Criação de Curso e Mentoria

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,34 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public Curso(String titulo, String descricao, int cargaHoraria) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,36 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+}
